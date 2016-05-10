c821a94ee71681396d3b9468a9efa2f9102fda1a
Sean Stanley <sean.stanley@terracor.ca>
2016-05-10 15:23:24 -0500

    Switch to SASS stylesheets Update to directory structure

--- collagify.js
+++ collagify.js
@@ -1,18 +1,27 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var sassMiddleware = require('node-sass-middleware')
+var path = require('path')
 var app = express()
 var API = require('./API/api')
 
+var sassOptions = {
+    src: path.join(__dirname, 'UI/style'),
+    dest: path.join(__dirname, 'Public/css'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/css'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}
+
 app.set('port', (process.env.PORT || 3333))
-
-app.use(express.static(__dirname + '/UI'))
-app.use(express.static(__dirname + '/Uploads'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-
 app.set('views', __dirname + '/Views');
 app.set('view engine', 'jade');
 
+app.use(express.static(__dirname + '/Public'))
+app.use(express.static(__dirname + '/Uploads'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(sassMiddleware(sassOptions))
 app.use('/api', API)
 app.use('/', function(req, res, next) {
   res.render("collagify");
@@ -25,4 +34,5 @@ app.use(function(req, res, next){
     res.status(404).render('404')
   }
 })
+
 app.listen(app.get('port'))
